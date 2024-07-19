343f4560f0572b82c37e1cc2601986b9e99e1649
ayahkhalilia <ayah.khalilia@stu.shenkar.ac.il>
2024-07-19 21:16:10 +0300

    fixed the modules so the render could work(testing)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const jsonserver=require("json-serve");
-const server=jsonserver.create();
-const router=jsonserver.router("./data/user-preferences.json");
-const middlewares=jsonserver.defaults();
+const jsonServer=require("json-server");
+const server=jsonServer.create();
+const router=jsonServer.router("./data/user-preferences.json");
+const middlewares=jsonServer.defaults();
 const port=process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
