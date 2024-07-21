7bc4f6158ed5a6df31ad703f1b962737c7048cf6
ayahkhalilia <ayah.khalilia@stu.shenkar.ac.il>
2024-07-21 22:36:17 +0300

    deleted the test file

--- app.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const app=express();
-const port=process.env.port || 3000
-const placesData=require("./places-list.json");
-app.get("/places",(req,res)=>{
-    res.json(placesData);
-});
-app.listen(port);
-console.log(`listening on port ${port}`);
